d6939d69be0db04e07dfcc845fed1c727ee6d8a1
Denilson Telaroli <ddts80@gmail.com>
2013-09-10 23:12:38 -0300

    Change to combine js

--- vplus-static/src/main/js/vplus/src/VPlusNamespace.js
+++ vplus-static/src/main/js/vplus/src/VPlusNamespace.js
@@ -1,4 +1,4 @@
 angular.module('VPlus.Config', []);
 angular.module('VPlus.Services', ['ngResource']);
-angular.module('VPlus.Utils', ['ngGrid', 'ui.bootstrap', 'ngTable']);
+angular.module('VPlus.Utils', ['ui.bootstrap', 'ngTable']);
 var VPlus = angular.module('VPlus', ['VPlus.Config', 'VPlus.Services', 'VPlus.Utils', 'ngSanitize']);
